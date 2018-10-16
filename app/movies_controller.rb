20304d52bc99038193159f08709d6fc41cea7212
Mika88 <mikaspivak@gmail.com>
2018-10-16 17:55:35 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,8 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 }
   # release_date == 1990
 
   Movie.create do |m|
-    binding.pry
+    m.title = "The Room"
+    m.release_date = 2003
   end
 end
 
